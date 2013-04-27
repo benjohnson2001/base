23abb60d1c0f13eb4f0cf3601414643d7e163831
Ben Johnson <benjohnson2001@gmail.com>
2013-04-26 20:11:32 -0400

    changed thingmenu font size to 16 so the icons look better and are closer together

--- build/thingmenu/config.h
+++ build/thingmenu/config.h
@@ -1,5 +1,5 @@
 static const Bool wmborder = False;
-static const char *font = "-misc-cloaked humanity bold-medium-r-normal--0-0-0-0-p-0-adobe-fontspecific";
+static const char *font = "-misc-cloaked humanity bold-medium-r-normal--16-0-0-0-p-0-adobe-fontspecific";
 static const char *normbgcolor = "#222222";
 static const char *normfgcolor = "#7D7D7D";
 static const char *pressbgcolor = "#222222";
