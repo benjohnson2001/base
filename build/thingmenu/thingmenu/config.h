1bf1996d4df27fc3d6affe3ad63c286d1fdc3643
Ben Johnson <benjohnson2001@gmail.com>
2013-06-08 08:05:28 -0400

    made modifications to thingmenu source code and changed urxvt configuration
    thingmenu
    -----------
    - reduced launcher icon height, before it was obstructing the terminal's green line border
       (also centered icon by adjusting offset in dwm source code)
    - removed thingmenu highlight color to prevent it from being stuck on that color, making the highlight area seem
    larger than the clicking area
    - changed button press color to red
    - also removed .git folders that were present in the thingmenu and clockmenu folders, this was causing problems with
    my git tracking, making the folders behave as broken submodules and independent repositories
    
    urxvt configuration
    --------------------
    - changed color of scrollbar to a darker color so it is less visible
    - changed terminal border color so the scrollbar contrast is better, also it looks much better

--- build/thingmenu/thingmenu/config.h
+++ build/thingmenu/thingmenu/config.h
@@ -3,10 +3,10 @@ static const char *font = "-misc-cloaked humanity bold-medium-r-normal--16-0-0-0
 static const char *normbgcolor = "#222222";
 static const char *normfgcolor = "#7D7D7D";
 static const char *pressbgcolor = "#222222";
-static const char *pressfgcolor = "#7D7D7D";
+static const char *pressfgcolor = "#DD4040";
 static const char *highlightbgcolor = "#222222";
-static const char *highlightfgcolor = "#CCCCCC";
+static const char *highlightfgcolor = "#7D7D7D";
 
 static float widthscaling = 1.0;
-static float heightscaling = 1.4;
+static float heightscaling = 1.3;
 
